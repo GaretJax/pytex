8a2cc3c399c850441c55e6545c5e1668e829e082
Jonathan Stoppani <jonathan@stoppani.name>
2012-10-10 09:00:10 +0200

    Support for spell checking

---
+++ pytex/commands/spellcheck.py
@@ -0,0 +1,54 @@
+import os
+import shlex
+import subprocess
+
+
+from pytex import utils
+from pytex.subcommands import Command
+
+
+class Spellcheck(Command):
+
+    name = 'spellcheck'
+
+    def execute(self, args):
+        base = os.path.realpath('.')
+        files = utils.find_files_of_type(
+            base,
+            ('tex',),
+            ('/build',),
+            ('preamble.tex',),
+        )
+
+        for f in files:
+            if self.spellcheck_file(f) is False:
+                self.logger.info('Process interrupted by user')
+                break
+        else:
+            self.logger.info('Done')
+
+
+    def spellcheck_file(self, file):
+        base = os.path.realpath('.')
+        dictionary = os.path.join(base, 'dictionary.dic')
+
+        cmd = shlex.split(self.config.get('spellcheck', 'command'))
+        cmd += [
+            '-p', dictionary,
+            file,
+        ]
+
+        if not os.path.exists(dictionary):
+            open(dictionary, 'w').close()
+
+        self.logger.debug(' '.join(cmd))
+
+        try:
+            subprocess.check_call(cmd)
+        except KeyboardInterrupt:
+            return False
+        except subprocess.CalledProcessError as e:
+            self.logger.error(e.output)
+            self.logger.error(e)
+
+command = Spellcheck()

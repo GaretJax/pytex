7835012bd08157f802af5774604300635c310220
Jonathan Stoppani <jonathan.stoppani@wsfcomp.com>
2014-05-02 16:52:14 +0200

    More experimentation on the acronyms checker front.

--- pytex/commands/typesetting.py
+++ pytex/commands/typesetting.py
@@ -10,7 +10,7 @@ from pytex.utils import find_files_of_type
 class FindUnreferencedAcronyms(Command):
 
     name = 'checkacronyms', 'ca'
-    acronym_regex = r'([A-Z]+-[A-Z]+|[A-Z]{2,})[a-z]*'
+    acronym_regex = r'[^a-zA-Z_\\-]((?:[A-Z]+[a-z-][A-Z]+|[A-Z]{2,})[a-z0-9]{0,3})[^a-zA-Z0-9_\\-]'
 
     def parser(self):
         parser = self.parser_class()
@@ -23,6 +23,18 @@ class FindUnreferencedAcronyms(Command):
         if not files:
             files = find_files_of_type(os.path.realpath('.'), 'tex')
 
+        ignoredb = self.config.get('acronyms', 'ignoredb')
+
+        ignored = set()
+
+        if os.path.exists(ignoredb):
+            with open(ignoredb) as fh:
+                for line in fh:
+                    line = line.strip()
+                    if line:
+                        ignored.add(line)
+
+
         stdscr = curses.initscr()
         curses.savetty()
         curses.noecho()
@@ -34,39 +46,43 @@ class FindUnreferencedAcronyms(Command):
                 self.replacements = []
                 contents = fh.read()
                 for m in re.finditer(self.acronym_regex, contents):
-                    ms, me = m.start(), m.end()
+                    ms, me = m.start(1), m.end(1)
                     token = contents
+
+                    abbr = token[ms:me]
+                    if abbr.endswith('s'):
+                        abbr = abbr[:-1]
+                    if abbr in ignored:
+                        continue
 
                     line = contents.count("\n", 0, ms)
 
-                    min_context = 10
-                    max_context = 500
+                    max_context = 20
+                    prefix = postfix = ''
 
                     # Isolate a single line
                     cs = contents.rfind("\n", 0, ms) + 1
                     ce = contents.find("\n", me)
 
+                    if ms - cs > max_context:
+                        cs = ms - max_context
+                        cs = contents.rfind(" ", 0, cs) + 1
+                        prefix = '...'
+
+                    if ce - me > max_context:
+                        ce = me + max_context
+                        ce = contents.find(" ", ce)
+                        postfix = '...'
+
+
                     char = ms - cs
 
                     ms -= cs
                     me -= cs
+
                     token = token[cs:ce]
 
-                    # For long lines, enforce max context
-                    #cs = max(0, ms - max_context)
-                    #ce = min(len(token), me + max_context)
-
-                    # Find a whitespace to split on, if not found, take the extremes
-                    #cs = token.find(" ", cs, max(0, ms - min_context)) + 1
-                    #ce = token.rfind(" ", me + min_context, ce)
-                    #if ce < 0:
-                    #    ce = min(len(token), me + max_context)
-
-                    #ms -= cs
-                    #me -= cs
-                    #token = token[cs:ce]
-
-                    self.print_token_color(stdscr, f, token, line, char, ms, me)
+                    self.print_token_color(stdscr, f, token, line, char, ms, me, prefix, postfix)
 
                     callbacks = {
                         'l': self.replace_lower,
@@ -85,7 +101,7 @@ class FindUnreferencedAcronyms(Command):
                             else:
                                 continue
                         else:
-                            callback(contents, m.start(), m.end())
+                            callback(contents, m.start(1), m.end(1))
                     except KeyboardInterrupt:
                         curses.resetty()
                         curses.nocbreak()
@@ -124,38 +140,37 @@ class FindUnreferencedAcronyms(Command):
 
 
     def replace_upper_plural(self, content, start, end):
+        name = content[start:end]
+        if name.endswith('s'):
+            name = name[:-1]
         self.replacements.append((
             start, end,
-            '\\Glspl{{{}}}'.format(content[start:end].lower())
+            '\\Glspl{{{}}}'.format(name.lower())
         ))
 
 
-    def print_token_color(self, stdscr, path, token, line, char, start, end):
-        prefix = ':{}:{}-{}'.format(line + 1, char + 1, char + end - start)
+    def print_token_color(self, stdscr, path, token, line, char, start, end, prefix, postfix):
+        path_prefix = ':{}:{}-{}'.format(line + 1, char + 1, char + end - start)
         abbr = token[start:end]
 
         stdscr.erase()
-        stdscr.addstr(1, 5, path+prefix)
-        prefix=''
+        stdscr.addstr(1, 5, path+path_prefix)
+        path_prefix=''
 
         chunksize = 80
         line = 0
 
         while token:
             chunk, token = token[:chunksize], token[chunksize:]
-            stdscr.addstr(3 + line, 5, chunk)
+            stdscr.addstr(3 + line, 5, prefix + chunk + postfix)
             if start > chunksize:
                 start -= chunksize
             elif start is not None:
-                stdscr.addstr(3 + line, 5 + start, abbr, curses.A_UNDERLINE | curses.A_BOLD)
+                stdscr.addstr(3 + line, 5 + start + len(prefix), abbr, curses.A_UNDERLINE | curses.A_BOLD)
                 start = None
 
             line += 1
 
-        #try:
-        #
-        #except curses.error:
-        #   pass
         stdscr.refresh()
 
 

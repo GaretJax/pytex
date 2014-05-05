cc2501eb723376736b7c522bb962c0f660a85d25
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-05-05 13:07:09 +0200

    Coalesce Events
    * Use a Notifier instead of ThreadedNotifier
    * Use a timeout of 100ms
    * On timeout, keep only the last event for each file
    * Use defaultdict for future extensibility

--- pytex/monitors/pyinotify.py
+++ pytex/monitors/pyinotify.py
@@ -3,20 +3,12 @@ from __future__ import absolute_import
 from pytex.monitors import base
 import pyinotify
 
-from operator import attrgetter
-
 import os
+import threading
+from collections import defaultdict
 
-class Stream(object):
-
+class CoalescerThread (threading.Thread):
     EVENT_MAPPINGS = {
-# TODO: The following OP flags are not yet defined. Do we need them?
-#       pyinotify.IN_ACCESS        : 0x00000001,  # File was accessed
-#       pyinotify.IN_CLOSE_WRITE   : 0x00000008,  # Writable file was closed
-#       pyinotify.IN_CLOSE_NOWRITE : 0x00000010,  # Unwritable file closed
-#       pyinotify.IN_OPEN          : 0x00000020,  # File was opened
-#       pyinotify.IN_DELETE_SELF   : 0x00000400,  # Self (watched item itself) was deleted
-#       pyinotify.IN_MOVE_SELF     : 0x00000800,  # Self (watched item itself) was moved
         pyinotify.IN_ATTRIB: base.FileModified,
         pyinotify.IN_CREATE: base.FileCreated,
         pyinotify.IN_MODIFY: base.FileModified,
@@ -24,9 +16,35 @@ class Stream(object):
         (pyinotify.IN_MOVED_FROM, pyinotify.IN_MOVED_TO): base.FileMoved,
     }
 
-    def __init__(self, callback):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.vm = pyinotify.WatchManager()
+        self.notifier = pyinotify.Notifier(self.vm, timeout=100)
+        self.flag = False
+
+    def monitor(self, path, callback):
         self.final_callback = callback
         self.linked_events = {}
+        self.event_buffer = defaultdict(list)
+        mask = 0
+        mask |= pyinotify.IN_CREATE | pyinotify.IN_MODIFY | pyinotify.IN_DELETE
+        mask |= pyinotify.IN_MOVED_FROM | pyinotify.IN_MOVED_TO
+        mask |= pyinotify.IN_ATTRIB
+        self.vm.add_watch(path, mask, self.handle_event, rec=True)
+
+    def run(self):
+        while not self.flag:
+            # Process events
+            self.notifier.process_events()
+            while self.notifier.check_events():
+                self.notifier.read_events()
+                self.notifier.process_events()
+
+            # Send callbacks for the last item of each event
+            for k,v in self.event_buffer.iteritems():
+                self.final_callback(v[-1])
+
+            self.event_buffer.clear()
 
     def handle_event(self, event):
         if hasattr(event, 'cookie'):
@@ -41,7 +59,8 @@ class Stream(object):
                 events.sort(key=attrgetter('mask'))
                 events = [(e.mask, e.path) for e in events]
                 key, paths = zip(*events)
-                self.final_callback(self.EVENT_MAPPINGS[key](*paths))
+                for e in self.EVENT_MAPPINGS[key](*paths):
+                    self.event_buffer[e.path] += [e]
         else:
             # Handle single event
             try:
@@ -49,25 +68,21 @@ class Stream(object):
             except KeyError:
                 print 'Ignoring event with opflag {}'.format(event.mask)
             else:
-                self.final_callback(event_class(os.path.join(event.path, event.name)))
-
-
-class Observer(object):
-
-    def __init__(self):
-        self.vm = pyinotify.WatchManager()
-        self.notifier = pyinotify.ThreadedNotifier(self.vm)
-
-    def start(self):
-        self.notifier.start()
-
-    def monitor(self, path, callback):
-        stream = Stream(callback)
-        mask = 0
-        mask |= pyinotify.IN_CREATE | pyinotify.IN_MODIFY | pyinotify.IN_DELETE
-        mask |= pyinotify.IN_MOVED_FROM | pyinotify.IN_MOVED_TO
-        mask |= pyinotify.IN_ATTRIB
-        self.vm.add_watch(path, mask, stream.handle_event, rec=True)
+                self.event_buffer[os.path.join(event.path, event.name)] += [event_class(os.path.join(event.path, event.name))]
 
     def stop(self):
-        self.notifier.stop()
+        self.flag = True
+        self.join()
+
+class Observer(object):
+    def __init__(self):
+        self.coalescer = CoalescerThread()
+
+    def start(self):
+        self.coalescer.start()
+
+    def monitor(self, path, callback):
+        self.coalescer.monitor(path, callback)
+
+    def stop(self):
+        self.coalescer.stop()
